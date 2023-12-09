c6a09c8dd732845ce61ca2097cbb9d4ede814d66
Klikmok <ribakov2007@icloud.com>
2023-12-10 00:24:01 +0500

    Третий пуш проекта на ревью

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -17,13 +17,14 @@ class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
+        User, on_delete=models.CASCADE, related_name='posts'
+    )
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True
     )
     group = models.ForeignKey(
         Group, on_delete=models.SET_NULL,
-        related_name='posts', blank=True, null=True,
+        related_name='posts', blank=True, null=True
     )
 
     def __str__(self):
